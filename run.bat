2c4638e6e89194ba1f7db7f5eb01e9fa9c4b1518
grant1004 <grant891004@gmail.com>
2023-03-29 15:48:33 +0800

    delete garbage

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-start cmd /k "cd /d E:\AllRepo\OurScheme\OurScheme && g++ -std=c++11 2023.3.28_new.cpp -o project2.exe && project2.exe"
+start cmd /k "cd /d E:\AllRepo\OurScheme\OurScheme && g++ -std=c++11 2023.3.29.cpp -o project2.exe && project2.exe"
 :: /k not close cmd 
 :: /c close cmd 
