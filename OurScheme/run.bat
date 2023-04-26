d1f66f3803b909defaef11cfae384196873a75cc
grant1004 <grant891004@gmail.com>
2023-04-26 16:11:08 +0800

    2023.4.26.cpp 是最新版

--- OurScheme/run.bat
+++ OurScheme/run.bat
@@ -1,4 +1,4 @@
 @echo off
-start cmd /k "cd /d E:\AllRepo\OurScheme\OurScheme && g++ -std=gnu++98 2023.4.23.cpp -o project3.exe && project3.exe "
+start cmd /k "cd /d E:\AllRepo\OurScheme\OurScheme && g++ -std=gnu++98 2023.4.25.cpp -o project3.exe && project3.exe "
 :: /k not close cmd 
 :: /c close cmd 
